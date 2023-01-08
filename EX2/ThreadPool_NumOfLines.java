d4d287db9997a951588c3e1f76a820ceb4d453d2
liorvi35 <liorvi35@gmail.com>
2023-01-08 16:35:27 +0200

    Update ThreadPool_NumOfLines.java

--- EX2/ThreadPool_NumOfLines.java
+++ EX2/ThreadPool_NumOfLines.java
@@ -1,8 +1,46 @@
-import java.util.concurrent.Callable;
+import java.io.*; // importing BufferReader and FileReader
+import java.util.concurrent.Callable; // importing Callable interface
 
-public class ThreadPool_NumOfLines implements Callable {
+/**
+ * this class is a helper-class that implements JAVA's callable interface
+ * used of counting number of lines inside of file using thread pool
+ *
+ * @authors Lior Vinman & Avraham Rahimov
+ * @version 10 January 2023
+ */
+public class ThreadPool_NumOfLines implements Callable<Integer> {
+
+    /**
+     * this field is representing a file name
+     */
+    private String name;
+
+    /**
+     * standard constructor method that gets a file name
+     * @param fileName - the name of the file
+     */
+    public ThreadPool_NumOfLines(String fileName) {
+        this.name = fileName;
+    }
+
+    /**
+     * this method is counting number of lines in file
+     * @return the number of lines in file
+     * @exception if error occurred when reading new lines
+     */
     @Override
-    public Object call() throws Exception {
-        return null;
+    public Integer call() throws Exception {
+        int total = 0;
+        BufferedReader reader = new BufferedReader(new FileReader(this.name));
+        try {
+            while (reader.readLine() != null) {
+                total++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            reader.close();
+        }
+        return total;
     }
 }
