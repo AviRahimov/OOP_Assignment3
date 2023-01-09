cef0bd3940c8e27619c7bf9ce7e68dc25df6c017
AviRahimov <98770917+AviRahimov@users.noreply.github.com>
2023-01-09 02:33:55 +0200

    Delete ThreadPool_NumOfLines.java

--- EX2/ThreadPool_NumOfLines.java
+++
@@ -1,8 +0,0 @@
-import java.util.concurrent.Callable;
-
-public class ThreadPool_NumOfLines implements Callable {
-    @Override
-    public Object call() throws Exception {
-        return null;
-    }
-}
