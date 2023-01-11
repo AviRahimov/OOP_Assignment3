66124f70d3c64101247e8bdd008b755e75147a2c
liorvi35 <liorvi35@gmail.com>
2023-01-11 15:48:28 +0200

    Update Ex2_1.java

--- EX2/Ex2_1.java
+++ EX2/Ex2_1.java
@@ -2,73 +2,121 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Random;
+import java.util.*;
+import java.util.concurrent.*;
 
-public class Ex2_1 {
-// We need to run the 3 functions except the first one and compare between their complexity time
-// and, we also need to create a UML diagram for this class.
+/**
+ * @author Lior Vinman, Avraham Rahimov
+ * @version 13 January 2023
+ */
+public class EX2_1 {
+
     /**
-     *
-     * @param n
-     * @param seed
-     * @param bound
-     * @return
-     * @throws IOException
+     * this function creates files
+     * @param n - number of files that should be created
+     * @param seed - for better random scattering
+     * @param bound - upper bound for maximum random number
+     * @return array with file names
      */
-    public static String[] createTextFiles(int n, int seed, int bound) throws IOException {
-        String files[] = new String[n];
+    public static String[] createTextFiles(int n, int seed, int bound) {
+        String[] files = new String[n];
         Random rand = new Random(seed);
-        for (int i = 0; i < n; i++) {
-            int x = rand.nextInt(bound);
-            File file = new File("file" + i);
-            PrintWriter out = new PrintWriter(file);
-            for (int j = 0; j < x; j++) {
-                out.println("Hello World" + i);
+        int randomLines = 0;
+        for(int i = 1; i <= n; i++) {
+            do {
+                randomLines = rand.nextInt(bound);
+            } while (randomLines == 0);
+            try {
+                File f = new File("file_" + i);
+                PrintWriter out = new PrintWriter(f);
+                for(int j = 0; j < randomLines; j++) {
+                    out.println("Hello World");
+                }
+                out.close();
+                files[i - 1] = f.getName();
+            } catch (FileNotFoundException | SecurityException e) {
+                e.printStackTrace();
             }
-            out.close();
-            files[i] = file.getName();
         }
         return files;
     }
 
     /**
-     *
-     * @param fileNames
-     * @return
-     * @throws IOException
+     * this function deletes files
+     * @param fileNames - array of file names that should be deleted
      */
-    public int getNumOfLines(String[] fileNames) throws IOException {
-        int count_lines = 0;
-        for(String file_name : fileNames){
-            Path file = Paths.get(file_name);
-            count_lines+=Files.lines(file).count();
+    public static void deleteTextFiles(String[] fileNames) {
+        for (String file : fileNames) {
+            File f = new File(file);
+            try {
+                f.delete();
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            }
         }
-        return count_lines;
     }
 
     /**
-     *
-     * @param fileNames
-     * @return
+     * this function counts num of lines in file
+     * @param fileNames - array of file names
+     * @return the total num of lines from all files
      */
-    public int getNumOfLinesThreads(String[] fileNames){
-        int count = 0;
-        for (String files : fileNames){
-            Thread_NumOfLines thread_lines = new Thread_NumOfLines(files);
-            thread_lines.setPriority(10);
-            thread_lines.run();
-            count+=Thread_NumOfLines.count_lines;
+    public static int getNumOfLines(String[] fileNames) {
+        int linesCount = 0;
+        for(String file : fileNames) {
+            Path pathToFile = Paths.get(file);
+            try {
+                linesCount += Files.lines(pathToFile).count();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        return count;
+        return linesCount;
     }
 
     /**
-     *
-     * @param fileNames
-     * @return
+     * this function counts num of lines in file, using threads
+     * @param fileNames - array of file names
+     * @return the total num of lines from all files
      */
-    public static int getNumOfLinesThreadPool(String[] fileNames){
-        int count = 0;
-        return count;
+    public static int getNumOfLinesThreads(String[] fileNames) {
+        int numLines = 0;
+        for(String file : fileNames) {
+            Thread_NumOfLines th = new Thread_NumOfLines(file);
+            th.run();
+            numLines = th.getTotal();
+        }
+        return numLines;
+    }
+
+    /**
+     * this function counts num of lines in file, using threadPool
+     * @param fileNames - array of file names
+     * @return the total num of lines from all files
+     */
+    public static int getNumOfLinesThreadPool(String[] fileNames) {
+        int total = 0;
+        ExecutorService pool = null;
+        try {
+            pool = Executors.newFixedThreadPool(fileNames.length);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        for(String file : fileNames) {
+            Callable<Integer> th = new ThreadPool_NumOfLines(file);
+            try {
+                Future<Integer> use = pool.submit(th);
+                total += use.get();
+            } catch (RejectedExecutionException | NullPointerException |
+                    CancellationException | ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            pool.shutdown();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+        return total;
     }
 }
